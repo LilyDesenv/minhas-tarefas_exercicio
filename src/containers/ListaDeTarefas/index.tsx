e65d9b1882444de761d3adb0850f6e395614c3e4
Alissandra <alissandra.o.t@gmail.com>
2024-04-08 17:10:14 -0300

    criação dos enums para tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,5 +1,44 @@
 import Tarefa from '../../components/Tarefa'
 import { Container, Titulo } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Titulo da Tarefa 1',
+    descricaro:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima iste tempore fuga vel quo perferendis animi laborum quod totam assumenda, adipisci soluta quos voluptas non expedita fugiat sit commodi?',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Titulo da Tarefa 2',
+    descricaro:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima iste tempore fuga vel quo perferendis animi laborum quod totam assumenda, adipisci soluta quos voluptas non expedita fugiat sit commodi?',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDO
+  },
+  {
+    titulo: 'Titulo da Tarefa 3',
+    descricaro:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima iste tempore fuga vel quo perferendis animi laborum quod totam assumenda, adipisci soluta quos voluptas non expedita fugiat sit commodi?',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Titulo da Tarefa 4',
+    descricaro:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima iste tempore fuga vel quo perferendis animi laborum quod totam assumenda, adipisci soluta quos voluptas non expedita fugiat sit commodi?',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Titulo da Tarefa 5',
+    descricaro:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima iste tempore fuga vel quo perferendis animi laborum quod totam assumenda, adipisci soluta quos voluptas non expedita fugiat sit commodi?',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
 
 const ListaDeTarefas = () => (
   <Container>
@@ -7,21 +46,16 @@ const ListaDeTarefas = () => (
       2 Tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;
     </Titulo>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            descricao={t.descricaro}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,6 @@
+export default {
+  vermelho: '#C23616',
+  verde: '#44BD32',
+  laranja: '#E1A32A',
+  amarelo: '#F0C11A'
+}
